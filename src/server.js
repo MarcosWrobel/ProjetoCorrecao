c787adb10c2b07fe0bc116a639a5b8304786838b
MarcosWrobel <77789486+MarcosWrobel@users.noreply.github.com>
2021-01-28 10:23:01 -0300

    Configuração do arquivo .env

--- src/server.js
+++ src/server.js
@@ -5,9 +5,10 @@ const routes = require('./routes');
 
 const app = express();
 
-console.log(process.env.APP_NAME);
-mongoose.connect('mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.xp1rw.mongodb.net:27017,cluster0-shard-00-01.xp1rw.mongodb.net:27017,cluster0-shard-00-02.xp1rw.mongodb.net:27017/projeto?ssl=true&replicaSet=atlas-l9o817-shard-0&authSource=admin&retryWrites=true&w=majority', {
-   
+
+mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`, {
+
+
 
     useNewUrlParser: true,
     useUnifiedTopology: true,
